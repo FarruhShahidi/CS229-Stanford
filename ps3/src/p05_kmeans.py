0cdc1f672b2340ac1bdfecd737964d405c3d21b8
Farruh Shahidi <farruh.shahidi@gmail.com>
2020-09-26 19:25:32 -0400

    ps3 is done

---
+++ ps3/src/p05_kmeans.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+
+
+
+
+
+def k_means(points, K):
+    """
+    Arguments:
+        - points: (M, N)
+    """
+    return 0
+
+
+
+
+
+
